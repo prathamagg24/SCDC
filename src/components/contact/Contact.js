91a39954113404ae4669aaf3512f2906611f2fd4
prathamagg24 <prathamagg24@gmail.com>
2023-07-18 01:45:43 +0530

    firebase connected

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,47 @@
 import React from "react";
 import "./Contact.css";
 import img from "./contact.jpg";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push } from 'firebase/database';
+
+const firebaseConfig = {
+  // Add your Firebase project configuration here
+  apiKey: "AIzaSyANLTob_x3b_GsIHjrPWzYkxiJh_o11FdA",
+  authDomain: "scdc-a24d7.firebaseapp.com",
+  projectId: "scdc-a24d7",
+  storageBucket: "scdc-a24d7.appspot.com",
+  messagingSenderId: "916394537213",
+  appId: "1:916394537213:web:03caafcf451e7eaac7ce94",
+  measurementId: "G-D6J6RH1ZV2"
+};
+
+initializeApp(firebaseConfig);
+const db = getDatabase();
+
 function Contact() {
+
+  
+    const handleSubmit =  (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+  
+      const name = formData.get('name');
+      const email = formData.get('email');
+      const message = formData.get('message');
+  
+      try {
+          push(ref(db, 'contacts'), {
+          name,
+          email,
+          message,
+        });
+  
+        // Form submission successful, handle any success actions here
+      } catch (error) {
+        console.log(error);
+      }
+    };
   return (
     <div>
 <div className="contact-text">
@@ -9,7 +49,7 @@ function Contact() {
 <div className="contact-subheading">Feel free to connect in case of queries</div>
 </div>
     <div className="contact">
-      <form action="https://submit-form.com/HZEivwMc">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           id="name"
