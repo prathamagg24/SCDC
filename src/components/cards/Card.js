79a2c332ce6896cc544782994009801604f869d4
Ish Kapoor <ishkapoor2000@gmail.com>
2023-08-25 19:09:58 +0000

    UI fix

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -2,16 +2,16 @@ import React from 'react'
 import "./Card.css"
 import svg1 from "../../images/Group 26.svg"
 function Card() {
-  return (
-    <div className="card_service">
-    <img src={svg1} className="svgicon" />
-    <div className="card_service_head">UI/UX Design</div>
-    <div className="card_service_body">
-    Our focus is on crafting intuitive and beautiful user experiences across all digital mediums. Whether it’s a website, app, or product.
-    </div>
-    <div className="card_service_btn">Contact Us</div>
-  </div>
-  )
+	return (
+		<div className="card_service">
+			<img src={svg1} className="svgicon" />
+			<div className="card_service_head">UI/UX Design</div>
+			<div className="card_service_body">
+				Our focus is on crafting intuitive and beautiful user experiences across all digital mediums. Whether it's a website, app, or product.
+			</div>
+			<div className="card_service_btn">Contact Us</div>
+		</div>
+	)
 }
 
 export default Card
--- src/components/testimonials/Test.js
+++ src/components/testimonials/Test.js
@@ -3,71 +3,86 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import "./Test.css";
 import movie from "./movie.mp4";
+
 export default class Testimonials extends Component {
-  handleVideoEnded = (event) => {
-    // Restart the video when it finishes playing
-    event.target.currentTime = 0;
-    event.target.play();
-  };
-  render() {
-    return (
-      <>
-  <div className="header_car">
-    What our customer are saying
-  </div>
-      <Carousel
-        showArrows={true}
-        infiniteLoop={true}
-        showThumbs={false}
-        showStatus={false}
-        autoPlay={true}
-        interval={6100}
-        stopOnHover={false}
-      >
-        <div className="carousel_scdc">
-          <div className="video_sec">
-            <video width="280" height="180" controls autoplay  onEnded={this.handleVideoEnded}>
-              <source src={movie} type="video/mp4" />
-            </video>
-          </div>
-          <div className="content_sec">
+	handleVideoEnded = (event) => {
+		// Restart the video when it finishes playing
+		event.target.currentTime = 0;
+		event.target.play();
+	};
 
-              “Our dedicated patient engagement app and web portal allow you to
-              access information instantaneously (no tedeous form, long calls,
-              or administrative hassle) and securely”
-          
-          </div>
-        </div>
-        <div className="carousel_scdc">
-          <div className="video_sec">
-            <video width="280" height="180" controls autoplay  onEnded={this.handleVideoEnded}>
-              <source src={movie} type="video/mp4" />
-            </video>
-          </div>
-          <div className="content_sec">
+	render() {
+		return (
+			<>
+				<div className="header_car">What our customers are saying</div>
+				<Carousel
+					showArrows={true}
+					infiniteLoop={true}
+					showThumbs={false}
+					showStatus={false}
+					autoPlay={true}
+					interval={6100}
+					stopOnHover={false}
+				>
+					<div className="carousel_scdc">
+						<div className="video_sec">
+							<video
+								width="280"
+								height="180"
+								controls
+								autoPlay
+								muted
+								onEnded={this.handleVideoEnded}
+							>
+								<source src={movie} type="video/mp4" />
+							</video>
 
-              “Our dedicated patient engagement app and web portal allow you to
-              access information instantaneously (no tedeous form, long calls,
-              or administrative hassle) and securely”
-          
-          </div>
-        </div>
-        <div className="carousel_scdc">
-          <div className="video_sec">
-            <video width="280" height="180" controls autoplay  onEnded={this.handleVideoEnded}>
-              <source src= {movie} type="video/mp4" />
-            </video>
-          </div>
-          <div className="content_sec">
-
-              “Our dedicated patient engagement app and web portal allow you to
-              access information instantaneously (no tedeous form, long calls,
-              or administrative hassle) and securely”
-          
-          </div>
-        </div>
-      </Carousel>
-      </>
-    );
-  }
+						</div>
+						<div className="content_sec">
+							“Our dedicated patient engagement app and web portal allow you to
+							access information instantaneously (no tedious form, long calls,
+							or administrative hassle) and securely”
+						</div>
+					</div>
+					<div className="carousel_scdc">
+						<div className="video_sec">
+							<video
+								width="280"
+								height="180"
+								controls
+								autoPlay
+								onEnded={this.handleVideoEnded}
+							>
+								<source src={movie} type="video/mp4" />
+							</video>
+						</div>
+						<div className="content_sec">
+							“Our dedicated patient engagement app and web portal allow you to
+							access information instantaneously (no tedious form, long calls,
+							or administrative hassle) and securely”
+						</div>
+					</div>
+					<div className="carousel_scdc">
+						<div className="video_sec">
+							<video
+								width="280"
+								height="180"
+								controls
+								autoPlay
+								onEnded={this.handleVideoEnded}
+							>
+								<source src={movie} type="video/mp4" />
+							</video>
+						</div>
+						<div className="content_sec">
+							“Our dedicated patient engagement app and web portal allow you to
+							access information instantaneously (no tedious form, long calls,
+							or administrative hassle) and securely”
+						</div>
+					</div>
+					{/* Repeat the above structure for each video */}
+				</Carousel>
+			</>
+		);
+	}
 }
